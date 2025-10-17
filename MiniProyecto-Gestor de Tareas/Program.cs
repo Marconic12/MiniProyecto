8fb172bd40642cdf4c2842211b6957b33a1a5f3e
Marco <masalas@uamv.edu.ni>
2025-10-17 14:39:39 -0600

    Agregue la funcion CompletarTareas por segunda vez

--- MiniProyecto-Gestor de Tareas/Program.cs
+++ MiniProyecto-Gestor de Tareas/Program.cs
@@ -125,3 +125,26 @@ namespace GestorTareasFuncional
 
     
 
+            } while (opcion != 0);
+        }
+
+        static void CompletarTarea()
+        {
+            ListarTareas();
+
+            if (descripciones.Count == 0) return;
+
+            Console.Write("\nIngrese el número de la tarea a completar: ");
+            if (int.TryParse(Console.ReadLine(), out int num) && num >= 1 && num <= descripciones.Count)
+            {
+                completadas[num - 1] = true;
+                Console.WriteLine("✅ Tarea marcada como completada.");
+            }
+            else
+            {
+                Console.WriteLine("⚠️ Número de tarea no válido.");
+            }
+        }
+
+    }
+}
