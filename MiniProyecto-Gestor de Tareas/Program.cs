236b7e7fd5740ee22fea662727300a1253fabb19
Marco <masalas@uamv.edu.ni>
2025-10-18 12:04:10 -0600

    corregiendo errores

--- MiniProyecto-Gestor de Tareas/Program.cs
+++ MiniProyecto-Gestor de Tareas/Program.cs
@@ -72,23 +72,7 @@ namespace GestorTareasFuncional
                 Console.WriteLine(" Tarea eliminada correctamente.");
             }
         }
-        static void CompletarTarea()
-        {
-            ListarTareas();
-
-            if (descripciones.Count == 0) return;
-
-            Console.Write("\nIngrese el número de la tarea a completar: ");
-            if (int.TryParse(Console.ReadLine(), out int num) && num >= 1 && num <= descripciones.Count)
-            {
-                completadas[num - 1] = true;
-                Console.WriteLine(" Tarea marcada como completada.");
-            }
-            else
-            {
-                Console.WriteLine(" Número de tarea no válido.");
-            }
-        }
+    
     }
 }
            
